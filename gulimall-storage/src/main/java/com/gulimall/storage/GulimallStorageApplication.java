42f01295dc62a3803da5d0b5e56b4c7d582c9a4d
XiaoNing <475529787@qq.ocm>
2022-10-23 14:30:06 +0800

    init: 项目结构初始化

---
+++ gulimall-storage/src/main/java/com/gulimall/storage/GulimallStorageApplication.java
@@ -0,0 +1,13 @@
+package com.gulimall.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallStorageApplication.class, args);
+    }
+
+}
---
+++ gulimall-storage/src/test/java/com/gulimall/storage/GulimallStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimall.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
